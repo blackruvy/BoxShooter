dce0494058900c28d08eb1f5c9b3cd7aa8a6b0f4
blackruvy <liuwei90@bu.edu>
2016-03-23 13:12:10 -0400

    First commit

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+    void onCollisionEnter(Collision newCollision)
+    {
+        if(newCollision.gameObject.tag == "Projectile")
+        {
+            GameManager.gm.RestartGame();
+        }
+    }
+}
